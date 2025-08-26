a1e6b3ed81c185ab78fe814713917b9d27b4a154
Jubaer Hossen <jubaerhossen042@gmail.com>
2025-08-26 18:16:37 +0600

    added blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
 import Navbar from './components/Navbar/Navbar'
 
 function App() {
@@ -9,6 +10,16 @@ function App() {
     <>
      
      <Navbar></Navbar>
+     
+     <div className="main-container flex text-center">
+      <div className="left-container w-[70%]">
+        <Blogs></Blogs>
+      </div>
+      <div className="right-container w-[30%]">
+        <h1>Reading time : 0</h1>
+        <h1>bookmarked count : 0</h1>
+      </div>
+     </div>
       
     </>
   )
